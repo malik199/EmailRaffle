e93cd0158cc2098ad81c25f42a365044aeb9329d
malik199 <ahmadm@ts-2.local>
2014-09-19 10:52:06 -0400

    Working JSON to ListView

--- src/com/malikahmad/emailraffle/ViewEntries.java
+++ src/com/malikahmad/emailraffle/ViewEntries.java
@@ -12,7 +12,6 @@ import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -22,15 +21,6 @@ import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 public class ViewEntries extends Activity {
-	// json string
-	private String jsonString = "{\"employee\":[{\"emp_name\":\"employee1\",\"emp_no\":\"101700\"},{\"emp_name\":\"employee2\",\"emp_no\":\"101701\"},{\"emp_name\":\"employee3\",\"emp_no\":\"101702\"},"
-			+ "{\"emp_name\":\"employee4\",\"emp_no\":\"101703\"},{\"emp_name\":\"employee5\",\"emp_no\":\"101704\"},{\"emp_name\":\"employee6\",\"emp_no\":\"101705\"},"
-			+ "{\"emp_name\":\"employee7\",\"emp_no\":\"101706\"},{\"emp_name\":\"employee8\",\"emp_no\":\"101707\"},{\"emp_name\":\"employee9\",\"emp_no\":\"101708\"},"
-			+ "{\"emp_name\":\"employee10\",\"emp_no\":\"101709\"},{\"emp_name\":\"employee11\",\"emp_no\":\"101710\"},{\"emp_name\":\"employee12\",\"emp_no\":\"101711\"},"
-			+ "{\"emp_name\":\"employee13\",\"emp_no\":\"101712\"},{\"emp_name\":\"employee14\",\"emp_no\":\"101713\"},{\"emp_name\":\"employee15\",\"emp_no\":\"101712\"}]}";
-
-	
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -80,14 +70,15 @@ public class ViewEntries extends Activity {
 				Toast.LENGTH_SHORT).show();
 		
 		try {
-			JSONObject jsonResponse = new JSONObject(jsonString);
-			JSONArray jsonMainNode = jsonResponse.optJSONArray("employee");
+			
+			JSONArray data = new JSONArray(b.toString());
 
-			for (int i = 0; i < jsonMainNode.length(); i++) {
-				JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
-				String name = jsonChildNode.optString("emp_name");
-				String number = jsonChildNode.optString("emp_no");
-				String outPut = name + "-" + number;
+			for (int i = 0; i < data.length(); i++) {
+
+				String email = data.getJSONObject(i).getString("EmailAddress");
+				String firstname = data.getJSONObject(i).getString("FirstName");
+				String lastname = data.getJSONObject(i).getString("LastName");
+				String outPut = (i+1) + ". " + email + " - " + firstname + " " + lastname;
 				employeeList.add(createEmployee("employees", outPut));
 			}
 		} catch (JSONException e) {
