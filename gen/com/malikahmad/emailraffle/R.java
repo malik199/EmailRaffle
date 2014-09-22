0abe69159e4f2b8b14e636f1375e169bd9888de2
malik199 <ahmadm@ts-2.local>
2014-09-22 14:15:31 -0400

    Deleting gif image

--- gen/com/malikahmad/emailraffle/R.java
+++ gen/com/malikahmad/emailraffle/R.java
@@ -1079,7 +1079,6 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int clock=0x7f020057;
         public static final int ic_launcher=0x7f020058;
-        public static final int timergif=0x7f020059;
     }
     public static final class id {
         public static final int RaffleDisplayName=0x7f050051;
