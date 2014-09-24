bb0dd203d725771664e936119aa82fe0c2983859
malik199 <ahmadm@ts-2.local>
2014-09-24 10:56:42 -0400

    got dialog box to work

--- gen/com/malikahmad/emailraffle/R.java
+++ gen/com/malikahmad/emailraffle/R.java
@@ -1268,7 +1268,9 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
+        public static final int already_raffle=0x7f0a001d;
         public static final int app_name=0x7f0a000d;
+        public static final int continue_raffle=0x7f0a001e;
         public static final int email_address=0x7f0a0014;
         public static final int end_raffle=0x7f0a0011;
         public static final int export_csv=0x7f0a001c;
@@ -1276,6 +1278,7 @@ containing a value of this type.
         public static final int get_winner=0x7f0a001a;
         public static final int hello_world=0x7f0a000e;
         public static final int last_name=0x7f0a0016;
+        public static final int new_name=0x7f0a001f;
         public static final int new_raffle=0x7f0a0010;
         public static final int number_entries=0x7f0a0017;
         public static final int raffle_name=0x7f0a0013;
--- src/com/malikahmad/emailraffle/MainActivity.java
+++ src/com/malikahmad/emailraffle/MainActivity.java
@@ -1,6 +1,10 @@
 package com.malikahmad.emailraffle;
 
+import java.io.File;
+
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,56 +12,93 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-
 public class MainActivity extends ActionBarActivity {
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-       
-    }
 
- 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.RaffleDisplayName) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    public void startRaffle(View view) {
-        // Do something in response to button click
-    	EditText myInput = (EditText)findViewById(R.id.raffleName); 
-    	
-       // Toast.makeText(this, myInput.getText().toString(), Toast.LENGTH_LONG).show();
-    	if(!isValid(myInput.getText().toString())) {
-    		myInput.setError("Please create a name that is more than 3 characters");
-    		myInput.requestFocus();
-    	} else {
-    		Intent intent = new Intent(this, AddContestantActivity.class);
-    		intent.putExtra("raffleName", myInput.getText().toString());
-    		startActivity(intent);
-    	}
-    }
-    
-    private boolean isValid(String pass) {
-    	if (pass != null && pass.length() > 2) {
-    		return true;
-    	} else {
-    		return false;
-    	}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.RaffleDisplayName) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void startRaffle(View view) {
+		// Do something in response to button click
+		final EditText myInput = (EditText) findViewById(R.id.raffleName);
+		String myInputString = myInput.getText().toString();
+		// Toast.makeText(this, myInput.getText().toString(),
+		// Toast.LENGTH_LONG).show();
+		if (!isValid(myInputString)) {
+			myInput.setError("Please create a name that is more than 3 characters");
+			myInput.requestFocus();
+		} else {
+
+			File file = getBaseContext().getFileStreamPath(myInputString);
+			if (file.exists()) {
+				//File exist so ask to continue old raffle.
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+						this);
+				alertDialogBuilder.setMessage(R.string.already_raffle);
+				alertDialogBuilder.setPositiveButton(R.string.new_raffle,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface arg0, int arg1) {
+								myInput.requestFocus();
+
+							}
+						});
+				alertDialogBuilder.setNegativeButton(R.string.continue_raffle,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								Intent intent = new Intent(
+										getApplicationContext(),
+										AddContestantActivity.class);
+								intent.putExtra("raffleName", myInput.getText()
+										.toString());
+								startActivity(intent);
+							}
+						});
+
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				alertDialog.show();
+			} else {
+				//File does not exist so create a new raffle.
+				Intent intent = new Intent(getApplicationContext(),
+						AddContestantActivity.class);
+				intent.putExtra("raffleName", myInput.getText().toString());
+				startActivity(intent);
+			}
+
+		}
+	}
+
+	private boolean isValid(String pass) {
+		if (pass != null && pass.length() > 2) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
